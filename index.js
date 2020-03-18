2c2af3e308485bca6ee6ee820a2da4180fd00c8d
brunobc <brunobezerrachaves@gmail.com>
2020-03-18 12:49:38 -0300

    Database

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+require('./routes/userRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
