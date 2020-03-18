60297f6fded2441da40deacb7016f308435e1e13
brunobc <brunobezerrachaves@gmail.com>
2020-03-18 13:21:54 -0300

    List users

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import userService from './services/user.service';
 
 function App() {
+  const [ users, setUsers ] = useState(null);
+
+  useEffect(() => {
+    if (!users) {
+      getUsers();
+    }
+  })
+
+  const getUsers = async () => {
+    let res = await userService.getAll();
+    setUsers(res);
+  }
+
+  const renderUser = user => {
+    return (
+      <li key={user._id} className="list__item user">
+        <h3 className="user__name">{user.name}</h3>
+        <h3 className="user__lastName">{user.lastName}</h3>
+      </li>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="list">
+        {(users && users.length > 0) ? (
+          users.map(user => renderUser(user))
+        ) : (
+          <p>No users found</p>
+        )}
+      </ul>
     </div>
   );
 }
