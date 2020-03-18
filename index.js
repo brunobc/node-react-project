2059e2166997a379012b09887c35a3386ae10caa
Bruno Bezerra Chaves <brunobc@BR-FZ130150.local>
2020-03-18 11:31:23 -0300

    backend configuration

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});
